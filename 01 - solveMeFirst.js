ee1c38f4b3639729f9f5d75f5bfe261511b55ecb
TAHER El Mehdi <57680648+MehdiTaher01@users.noreply.github.com>
2021-11-29 12:47:18 +0100

    solveMeFirst
    function solveMeFirst to compute the sum of two integers.

---
+++ 01 - solveMeFirst.js
@@ -0,0 +1,36 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+//The function solveMeFirst to compute the sum of two integers.
+function solveMeFirst(a, b) {
+      while(a){
+        b++;
+        a--;
+    }
+    return b;  
+}
+
+function main() {
+    var a = parseInt(readLine());
+    var b = parseInt(readLine());;
+
+    var res = solveMeFirst(a, b);
+    console.log(res);
+}
