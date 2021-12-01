6b44e577d8f5335daa18cce271ee5e07b469ab1c
TAHER El Mehdi <57680648+MehdiTaher01@users.noreply.github.com>
2021-12-01 13:34:28 +0100

    compareTriplets

---
+++ 03 - compareTriplets.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a, b) {
+  let aScore, bScore;
+  aScore = bScore = 0;
+  a.forEach((e, i) => (a[i] > b[i] ? ++aScore : a[i] < b[i] ? ++bScore : null));
+  return [aScore, bScore];
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const a = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((aTemp) => parseInt(aTemp, 10));
+
+  const b = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((bTemp) => parseInt(bTemp, 10));
+
+  const result = compareTriplets(a, b);
+
+  ws.write(result.join(" ") + "\n");
+
+  ws.end();
+}
